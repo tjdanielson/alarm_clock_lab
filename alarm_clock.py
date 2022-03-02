ea921782fdcff0c17f27031be0474734d6ae3e0a
tjdanielson <tessajdanielson@gmail.com>
2022-03-02 11:45:04 -0600

    Import alarm clock to main and call methods from main

--- alarm_clock.py
+++ alarm_clock.py
@@ -2,20 +2,22 @@
 class AlarmClock:
 
     def __init__(self):
-        self.current_time = ''
+        self.current_time = '12:00 AM'
         self.alarm_on = False
         self.alarm_time = ''
 
     def set_time(self):
-        self.current_time = input('What time is it?')
-        print(self.current_time)
+        self.current_time = input('What time is it? ')
+        print('The current time is: ' + self.current_time)
 
     def toggle_alarm(self):
         if self.alarm_on == False:
             self.alarm_on = True
+            print('Your alarm is now ON')
         else:
             self.alarm_on = False
+            print('Your alarm is now OFF')
     
     def set_alarm(self):
-        self.alarm_time = input('What time do you want your alarm to ring?')
+        self.alarm_time = input('What time do you want your alarm to ring? ')
         print('Your alarm is set for: ' + self.alarm_time)
---
+++ main.py
@@ -0,0 +1,15 @@
+from alarm_clock import AlarmClock
+
+alarm_clock_one = AlarmClock()
+
+#print original current clock's time to the terminal
+print(alarm_clock_one.current_time)
+
+#change the time
+alarm_clock_one.set_time()
+
+#toggle alarm clock's on/off switch (and print new on/off status to terminal)
+alarm_clock_one.toggle_alarm()
+
+#set the alarm time
+alarm_clock_one.set_alarm()
