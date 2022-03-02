4644f00842c264f878fab6557f01eefd07cd7668
tjdanielson <tessajdanielson@gmail.com>
2022-03-02 11:36:29 -0600

    Create alarm clock methods

--- alarm_clock.py
+++ alarm_clock.py
@@ -5,3 +5,17 @@ class AlarmClock:
         self.current_time = ''
         self.alarm_on = False
         self.alarm_time = ''
+
+    def set_time(self):
+        self.current_time = input('What time is it?')
+        print(self.current_time)
+
+    def toggle_alarm(self):
+        if self.alarm_on == False:
+            self.alarm_on = True
+        else:
+            self.alarm_on = False
+    
+    def set_alarm(self):
+        self.alarm_time = input('What time do you want your alarm to ring?')
+        print('Your alarm is set for: ' + self.alarm_time)
